0a3c261bc6b6b00f57a2198c51c132a0564d5739
Delta <khanrahib250@gmail.com>
2026-02-20 22:48:29 +0530

    Fixed port for render

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ connectDB();
 app.use("/users", userRoutes);
 app.use("/candidate", candidateRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
