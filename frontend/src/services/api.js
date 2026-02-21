3e2404efd016f041760ba93e1bdaa31b5aa0eae4
Delta <khanrahib250@gmail.com>
2026-02-21 12:18:39 +0530

    Sync api exports

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,37 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export default API_URL;
+
+// LOGIN
+export const loginUser = async (loginData) => {
+  const response = await fetch(`${API_URL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(loginData)
+  });
+
+  return response.json();
+};
+
+
+// GET CANDIDATES
+export const getCandidates = async () => {
+  const res = await fetch(`${API_URL}/candidate/results`);
+  return res.json();
+};
+
+
+// VOTE
+export const voteCandidate = async (candidateId, token) => {
+  const res = await fetch(`${API_URL}/candidate/vote`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify({ candidateId })
+  });
+
+  return res.json();
+};
