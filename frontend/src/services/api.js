a20ba543cd4e93ef01c9d72f83962d1e34867ca7
Delta <khanrahib250@gmail.com>
2026-02-21 11:58:50 +0530

    Fixed API export

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+
+// LOGIN
 export const loginUser = async (loginData) => {
   const response = await fetch(`${API_URL}/users/login`, {
     method: "POST",
@@ -10,4 +12,26 @@ export const loginUser = async (loginData) => {
   });
 
   return response.json();
+};
+
+
+// GET CANDIDATES
+export const getCandidates = async () => {
+  const res = await fetch(`${API_URL}/candidate/results`);
+  return res.json();
+};
+
+
+// VOTE
+export const voteCandidate = async (candidateId, token) => {
+  const res = await fetch(`${API_URL}/candidate/vote`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify({ candidateId })
+  });
+
+  return res.json();
 };
